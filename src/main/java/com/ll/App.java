9ca2ac4b34c18130f0603e0d08a1f483e0fb5c2e
깃waimi3169 <waimi3169@naver.com>
2023-04-16 20:27:51 +0900

    4단계 구현 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -10,8 +10,11 @@ public class App {
     }
     public void run() {
         System.out.println("== 명언 앱 ==");
+
+        long lastWiseSayingId = 1;
+
         while(true) {
-            System.out.print("명령)");
+            System.out.print("명령) ");
             String command = sc.nextLine().trim();
 
             if(command.equals("종료")) {
@@ -21,7 +24,8 @@ public class App {
                 String content = sc.nextLine().trim();
                 System.out.print("작가 : ");
                 String authorName = sc.nextLine().trim();
-                System.out.printf("1번 명언이 등록되었습니다.");
+                System.out.printf("%d번 명언이 등록되었습니다.\n",lastWiseSayingId);
+                lastWiseSayingId++;
             }
         }
     }
